13f7b35ec58236494111a36606d93d3bf74ce53e
AndyQi777 <49790210+AndyQi777@users.noreply.github.com>
2019-04-21 08:54:38 +0800

    Update cat.cpp

--- cat.cpp
+++ cat.cpp
@@ -27,6 +27,7 @@ class cat : public contract
     cat(uint64_t account_id)
         : contract(account_id)
         , accounts(_self, _self)
+        , packets(_self, _self)
     {
     }
 
@@ -43,6 +44,14 @@ class cat : public contract
         uint64_t owner = get_trx_sender();
         auto it = accounts.find(owner);
 
+        //位注册用户免费赠送一只电子猫，共五种不同属性1-5，随机抽取
+        int InitialCat = 0;
+        int64_t block_num = get_head_block_num();
+        std::string random_str = std::to_string(asset_id) + std::to_string(block_num);
+        checksum160 sum160;
+        ripemd160(const_cast<char *>(random_str.c_str()), random_str.length(), &sum160);
+        InitialCat = (int(sum160.hash)) % 5;
+
         // 三种猫粮，level1-3由普通到高级
         int catfoodlevel1 = 1;
         int catfoodlevel2 = 3;
@@ -51,7 +60,7 @@ class cat : public contract
         int specialkind1 = 6.25;
         int specialkind2 = 6.75;
         
-        //循环n次，n为玩家所拥有的探寻猫粮的次数
+        //循环n次，n为玩家所拥有的探寻猫粮的次数(虚拟货币数)
         int n = amount.amount;
 
         //自此之后，amount.amount表示玩家所拥有的猫粮数
@@ -107,8 +116,38 @@ class cat : public contract
                 accounts.modify(it, 0, [&](auto &o) { o.chances.emplace_back(amount); });
             }
         }
+        int64_t &p = amount.amount;
+        uint64_t &q = amount.asset_id;
     }
 
+    //@abi action
+    //@abi payable
+    void feed(std::string pubkey,contract_asset amount)
+    {
+        //查验公钥
+        std::string prefix("GXC");
+        const size_t prefix_len = prefix.size();
+        graphene_assert(pubkey.size() > prefix_len, "invalid public key");
+        graphene_assert(pubkey.substr(0, prefix_len) == prefix, "invalid public key");
+
+        int64_t total_amount = get_action_asset_amount();
+        uint64_t asset_id = get_action_asset_id();
+        uint64_t owner = get_trx_sender();
+        //键入投喂的猫粮数
+        packets.emplace(owner, [&](auto &o) {
+            o.owner = owner;
+            o.pub_key = pubkey;
+            o.total_amount = contract_asset{total_amount, asset_id};
+            if (amount.amount >= total_amount){
+                int64_t difference = amount.amount - total_amount;
+                o.chances.emplace_back(difference);
+            }else{
+                graphene_assert(amount.amount < total_amount, "invalid");
+            }
+                
+        });
+
+    }
   private:
     //@abi table account i64
     struct account {
@@ -124,6 +163,19 @@ class cat : public contract
 
     account_index accounts;
 
+        struct packet {
+        uint64_t                owner;
+        std::string             pub_key;
+        contract_asset          total_amount;
+        uint32_t                number;
+        vector<int64_t>         chances;
+
+        uint64_t primary_key() const { return owner; }
+
+        GRAPHENE_SERIALIZE(packet, (owner)(pub_key)(total_amount)(number)(chances))
+    };
+    typedef graphene::multi_index<N(packet), packet> packet_index;
+    packet_index            packets;
 };
 
-GRAPHENE_ABI(cat, (deposit))
+GRAPHENE_ABI(cat, (deposit)(feed))
